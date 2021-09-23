bea42205631c749c93d2c8f6c39a3ebd4eb91507
Alan Kilby <alan.kilby.cerquera@gmail.com>
2021-09-23 09:20:51 +0200

    Jump + Player Movement

---
+++ GoldenGun_GB/Assets/AK_Scripts/AK_PlayerScripts/AK_GroundCheck.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AK_GroundCheck : MonoBehaviour
+{
+    public bool isGrounded;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+            isGrounded = true;
+
+        else
+            isGrounded = false;
+    }
+
+}
--- GoldenGun_GB/Assets/AK_Scripts/AK_PlayerScripts/AK_PlayerMovement.cs
+++ GoldenGun_GB/Assets/AK_Scripts/AK_PlayerScripts/AK_PlayerMovement.cs
@@ -8,21 +8,54 @@ public class AK_PlayerMovement : MonoBehaviour
     
 
     public float playerSpeed;
+    public float jumpForce;
+
     float horizontalMovement;
+
+    public bool isGrounded;
+
+    public Transform groundCheck;
+
+    public LayerMask groundLayer;
+    int groundLayerInt;
 
     void Start()
     {
-        playerRB = gameObject.GetComponent<Rigidbody2D>();   
+        playerRB = gameObject.GetComponent<Rigidbody2D>();
+        groundLayerInt = groundLayer.value;
     }
     void Update()
     {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position,0.45f, groundLayerInt);
+        
+
         horizontalMovement = Input.GetAxisRaw("Horizontal") * playerSpeed;
 
+        //isGrounded = groundCheck.isGrounded;
+        
+
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            PlayerJump();
+        }
+
         MovePlayer(horizontalMovement);
+        
     }
 
     void MovePlayer(float _horizontalMovement)
     {
         playerRB.velocity = new Vector2(_horizontalMovement, playerRB.velocity.y);
+
+    }
+
+    void PlayerJump()
+    {
+        playerRB.velocity = new Vector2(playerRB.velocity.x, jumpForce);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, 0.45f);
     }
 }
