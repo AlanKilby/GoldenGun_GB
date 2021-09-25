29ba494305bce558721bb7bc7fbf25b19d813443
Alan Kilby <alan.kilby.cerquera@gmail.com>
2021-09-25 17:46:08 +0200

    Boss Fight WiP + Tileset Rework

---
+++ GoldenGun_GB/Assets/AK_Scripts/AK_EnemyScripts/AK_BossFightScript.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class AK_BossFightScript : MonoBehaviour
+{
+    public float maxTime;
+
+    float shootMoment;
+    float shootMoment2;
+
+    public float timeLeft;
+
+
+    public Transform playerShootingPoint;
+    public Transform enemyShootingPoint;
+
+    public Sprite victoryScreen;
+    public Sprite defeatScreen;
+
+    public Sprite playAgainScreenVictory;
+    public Sprite playAgainScreenDefeat;
+
+    bool canShoot;
+    bool endScreen;
+
+    public Sprite getReady;
+    public Sprite shoot;
+
+    SpriteRenderer spriteRend;
+
+    public string levelScene;
+    public string mainMenu;
+
+    public GameObject playerBullet;
+    public GameObject enemyBullet;
+
+
+
+    private void Start()
+    {
+        //timesUpText.SetActive(false);
+        
+        timeLeft = maxTime;
+        shootMoment = Random.Range(1, maxTime - 1);
+        shootMoment2 = shootMoment - 0.5f;
+        spriteRend = GetComponent<SpriteRenderer>();
+        endScreen = false;
+    }
+
+    private void Update()
+    {
+
+        if (timeLeft > shootMoment)
+        {
+            timeLeft -= Time.deltaTime;
+            spriteRend.sprite = getReady;
+        }
+
+        if (timeLeft > shootMoment2 && timeLeft < shootMoment)
+        {
+            spriteRend.sprite = shoot;
+            if (Input.GetKeyDown(KeyCode.O) && canShoot)
+            {
+                Instantiate(playerBullet, playerShootingPoint.position, Quaternion.identity);
+                Victory();
+            }
+        }
+        if (timeLeft < shootMoment2)
+        {
+            spriteRend.sprite = null;
+            if (Input.GetKeyDown(KeyCode.O) && canShoot)
+            {
+                Instantiate(enemyBullet, enemyShootingPoint.position, Quaternion.identity);
+                Defeat();
+            }
+        }
+
+        if (endScreen)
+        {
+            if (Input.GetKeyDown(KeyCode.O))
+            {
+                SceneManager.LoadScene(levelScene);
+            }
+
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                SceneManager.LoadScene(mainMenu);
+            }
+        } 
+    }
+
+    void Victory()
+    {
+        canShoot = false;
+        StartCoroutine("VictoryCor");
+    }
+
+    void Defeat()
+    {
+        canShoot = false;
+        StartCoroutine("DefeatCor");
+    }
+
+    IEnumerator DefeatCor() 
+    {
+        spriteRend.sprite = defeatScreen;
+
+        yield return new WaitForSeconds(3f);
+
+        spriteRend.sprite = playAgainScreenDefeat;
+
+        endScreen = true;
+
+        yield return null;
+    }
+
+    IEnumerator VictoryCor()
+    {
+        spriteRend.sprite = victoryScreen;
+
+        yield return new WaitForSeconds(3f);
+
+        spriteRend.sprite = playAgainScreenVictory;
+
+        endScreen = true;
+
+        yield return null;
+    }
+}
---
+++ GoldenGun_GB/Assets/AK_Scripts/AK_EnemyScripts/AK_TimerBar.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AK_TimerBar : MonoBehaviour
+{
+    Image timerBar;
+
+    public float maxTime;
+
+    float shootMoment;
+    float shootMoment2;
+
+    public float timeLeft;
+
+
+    public GameObject timesUpText;
+
+    public GameObject shootIndicator;
+
+
+    private void Start()
+    {
+        //timesUpText.SetActive(false);
+        timerBar = GetComponent<Image>();
+        timeLeft = maxTime;
+        shootMoment = Random.Range(1, maxTime - 1);
+        shootMoment2 = shootMoment - 0.5f;
+    }
+
+    private void Update()
+    {
+        
+
+        if(timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+            timerBar.fillAmount = timeLeft / maxTime;
+        }
+
+        if(timeLeft > shootMoment2 && timeLeft < shootMoment)
+        {
+            shootIndicator.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.O))
+            {
+
+            }
+        }
+        if(timeLeft < shootMoment2)
+        {
+            shootIndicator.SetActive(false);
+        }
+    }
+}
