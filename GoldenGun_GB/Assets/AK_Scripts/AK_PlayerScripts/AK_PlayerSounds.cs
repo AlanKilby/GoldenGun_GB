c38adfa706817feb93d8a58ad7d31dba7bd1644d
Alan Kilby <alan.kilby.cerquera@gmail.com>
2021-09-26 17:14:31 +0200

    Level 1 fixes

---
+++ GoldenGun_GB/Assets/AK_Scripts/AK_PlayerScripts/AK_PlayerSounds.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AK_PlayerSounds : MonoBehaviour
+{
+    public AudioClip TAKE_DAMAGE;
+    public AudioClip SHOOT;
+    public AudioClip DEATH;
+    public AudioClip PICK_UP;
+    public AudioClip KILL;
+}
